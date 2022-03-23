cc0f8e78d4e02ba5a5975fbcc2e38f2fd8d53a04
Yuzhou Liu <yuzhou.liu@mongodb.com>
2022-03-23 12:55:07 -0400

    working UI

--- internal/server/ui/app.js
+++ internal/server/ui/app.js
@@ -1,8 +1,8 @@
-'use strict'
+"use strict";
 
-function apiPrefix() {
-    return "/api/v1beta1/distributions"
-}
+let itemCount = 0;
+const apiPrefix = "/api/v1beta1/distributions";
+const itemPrefix = "item-";
 
 async function getJson(url = "") {
     const response = await fetch(url, {
@@ -12,14 +12,14 @@ async function getJson(url = "") {
         }
     })
     .catch(error => {
-        throw new Error(error)
+        throw new Error(error);
     })
 
     const data = await response.json();
 
     if (!response.ok) {
         const message = `GET ${url} returned error: ${response.status} ${data.status}`;
-        throw new Error(message)
+        throw new Error(message);
     }
     return data;
 }
@@ -34,76 +34,128 @@ async function postJson(url = "", postData = {}) {
         body: JSON.stringify(postData),
     })
     .catch(error => {
-        throw new Error(error)
+        throw new Error(error);
     })
 
     const data = await response.json();
 
     if (!response.ok) {
         const message = `POST ${url} returned error: ${response.status} ${data.status}`;
-        throw new Error(message)
+        throw new Error(message);
     }
     return data;
 }
 
+function appendOutput(header = "", details = "", data = {}) {
+    // Add item to side nav bar
+    let navBar = document.getElementById("nav-bar");
+
+    let linkNode = document.createElement("a");
+    linkNode.setAttribute("class", "list-group-item list-group-item-action");
+    linkNode.href = "#" + itemPrefix + itemCount;
+    linkNode.innerHTML = header;
+
+    navBar.appendChild(linkNode);
+
+    // Add to output box
+    let outputBox = document.getElementById("output-box");
+
+    let headerNode = document.createElement("h4");
+    headerNode.id = itemPrefix.concat(itemCount);
+    headerNode.innerHTML = header;
+
+    let paragraphNode = document.createElement("p")
+    paragraphNode.innerHTML = details;
+
+    let preNode = document.createElement("pre");
+    preNode.textContent = JSON.stringify(data, undefined, 2);
+
+    outputBox.appendChild(headerNode);
+    outputBox.appendChild(paragraphNode);
+    outputBox.appendChild(preNode);
+
+    itemCount++;
+
+    $('[data-spy="scroll"]').each(function () {
+        $(this).scrollspy('refresh');
+    })
+}
+
 async function populateDistributionDropdowns() {
-    console.log("YUZHOU DEBUG in populateDistributionDropdowns")
+    let createInvalidationDropdown = document.getElementById("create-invalidation-distribution");
+    let getInvalidationDropdown = document.getElementById("get-invalidation-distribution");
 
-    let createInvalidationDropdown = document.getElementById("create-invalidation-distribution")
-    let getInvalidationDropdown = document.getElementById("get-invalidation-distribution")
-
-    await getJson(apiPrefix())
+    await getJson(apiPrefix)
     .then(data => {
         data.distributions.forEach(distribution => {
-        let option = document.createElement("option")
-        option.text = distribution
-        option.value = distribution
+            let option = document.createElement("option");
+            option.text = distribution;
+            option.value = distribution;
 
-        createInvalidationDropdown.appendChild(option.cloneNode(true))
-        getInvalidationDropdown.appendChild(option.cloneNode(true))
+            createInvalidationDropdown.appendChild(option.cloneNode(true));
+            getInvalidationDropdown.appendChild(option.cloneNode(true));
         })
     })
     .catch(error => {
-        console.log(error.message);
+        appendOutput("Error Populating Distributions", "", error.message);
     })
+
+    document.getElementById("loading").style.visibility="hidden";
 }
 
 async function createInvalidation() {
-    console.log("YUZHOU DEBUG in createInvalidation")
+    // show loading spinner
+    document.getElementById("loading").style.visibility="visible";
 
+    // construct the POST request
     let distribution = document.getElementById("create-invalidation-distribution").value;
-    let url = apiPrefix().concat("/", distribution, "/invalidations");
+    let url = apiPrefix.concat("/", distribution, "/invalidations");
 
     let commaSeparatedPaths = document.getElementById("create-invalidation-paths").value;
     let pathsArr = commaSeparatedPaths.split(",").map(function(item) {
-        return item.trim();
+        return item.trim().replace(/^"(.*)"$/, "$1");   // remove surrounding whitespace and quotes if any
     });
 
+    let details = "<b>Distribution:</b> " + distribution + "<br />";
+    details = details + "<b>Paths:</b> " + pathsArr.join(",") + "<br />";
+
+    // send POST request
     await postJson(url, { paths: pathsArr })
     .then(data => {
-        console.log(data)
+        appendOutput("Create", details, data);
     })
     .catch(error => {
-        console.log(error.message);
+        appendOutput("Create Error", details, error.message);
     });
+
+    // hide loading spinner
+    document.getElementById("loading").style.visibility="hidden";
 }
 
-async function getInvalidation() {
-    console.log("YUZHOU DEBUG in getInvalidation")
 
+
+async function getInvalidation() {
+    // show loading spinner
+    document.getElementById("loading").style.visibility="visible";
+
+    // construct the GET request
     let distribution = document.getElementById("get-invalidation-distribution").value;
     let invalidationID = document.getElementById("get-invalidation-id").value;
-    // remove surrounding quotes if any
-    invalidationID = invalidationID.replace(/^"(.*)"$/, "$1");
-    let url = apiPrefix().concat("/", distribution, "/invalidations/", invalidationID);
+    invalidationID = invalidationID.replace(/^"(.*)"$/, "$1");  // remove surrounding quotes if any
+    let url = apiPrefix.concat("/", distribution, "/invalidations/", invalidationID);
 
-    console.log("YUZHOU DEBUG invalidationID: " + invalidationID)
+    let details = "<b>Distribution:</b> " + distribution + "<br />";
+    details = details + "<b>Invalidation ID:</b> " + invalidationID + "<br />";
 
+    // send GET request
     await getJson(url)
     .then(data => {
-        console.log(data)
+        appendOutput("Get", details, data);
     })
     .catch(error => {
-        console.log(error.message);
+        appendOutput("Get Error", details, error.message);
     });
+
+    // hide loading spinner
+    document.getElementById("loading").style.visibility="hidden";
 }
